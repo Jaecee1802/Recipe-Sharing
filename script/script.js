3c07b3746f913170857b184704aa81b5e6bd763b
JC Banzuela <162450651+Jaecee1802@users.noreply.github.com>
2024-11-01 20:55:52 +0800

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,11 @@
+let featuredRecipesTitle = document.getElementById("featuredRecips");
+let recpName = document.getElementById("recipeName");
+let recpName2 = document.getElementById("recipeName2");
+let recpName3 = document.getElementById("recipeName3");
+
+featuredRecipesTitle.style.color = "whitesmoke";
+featuredRecipesTitle.style.fontFamily = "Lora";
+
+recpName.innerHTML = "Homemade";
+recpName2.innerHTML = "Ready to Cook";
+recpName3.innerHTML = "Vegan"
